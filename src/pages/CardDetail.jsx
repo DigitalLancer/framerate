53c5148ab7ef6f29f0266a432999d43b782ba84f
DigitalLancer <ahmetsalimusu@yahoo.com>
2025-04-01 18:35:31 +0300

    Code cleaning

--- src/pages/CardDetail.jsx
+++ src/pages/CardDetail.jsx
@@ -3,14 +3,9 @@ import DetailBanner from "../components/Detail/DetailBanner";
 import useFetch from "../hooks/useFetch";
 import DetailContent from "../components/Detail/DetailContent";
 import '../style.css'
-import { useState, useEffect } from 'react';
-import StarRating from "../components/StarRating/StarRating";
-import AntModal from "../components/AntModal/AntModal";
 
 function CardDetail(){
     const param=useParams();
-    const [myState, setMyState] = useState(null);
-    const [rating, setRating] = useState(0)
     const {id}=param;
     const movieData = useFetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5a8fc6c2d88fb7b5d58014977ad0025e`);
     const creditsData= useFetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=5a8fc6c2d88fb7b5d58014977ad0025e`);
@@ -21,7 +16,7 @@ function CardDetail(){
         <>
             <div className="container">
                 <div>
-                    {movieData && creditsData && accountState && <DetailBanner movie={movieData} credits={creditsData} accountState={accountState} setMyState={setMyState} compType="movie"/>}
+                    {movieData && creditsData && accountState && <DetailBanner movie={movieData} credits={creditsData} accountState={accountState}  compType="movie"/>}
                 </div>
                 <div className="cast-info">
                     {creditsData && movieData && providersData && <DetailContent credits={creditsData} movie={movieData} providers={providersData}/>}
